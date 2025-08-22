89a1cda3a2796aacd208c5a5e5989001315e50f6
Jonatan Eduardo Ramirez Lopez <yonylpz01@hotmail.com>
2025-08-22 08:50:23 -0600

    ng lint --fix

--- src/app/domains/shared/directives/highlight.directive.ts
+++ src/app/domains/shared/directives/highlight.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, inject } from '@angular/core';
+import { Directive, ElementRef, inject, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[highlight]',
   standalone: true
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
 
   element = inject(ElementRef);
 
